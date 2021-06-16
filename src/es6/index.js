0f2e3b507d75914fce8cfb75e0bf2634f6bd64d7
ULISES <nexu_2001@hotmail.com>
2021-06-16 11:00:38 -0500

    clase °5: Clases, Módulos y Generadores

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,48 +1,31 @@
-let name="oscar";
-let age=32;
-//es5
-obj={name: name, age:age};
-//es6
-obj2={name ,age};
-
-console.log(obj2)
-
-//arrow functions
-
-const names=[
-    {name:'oscar',age='32'},
-    {name:'yesica',age='27'}
-]
-
-let listOfnames= names.map(function(item){
-    console.log(item.name);
+class calculator{
+    constructor(){
+        this.valueA= 0;
+        this.valueB=0;
+    }
+    sum(valueA,valueB){
+        this.valueA=valueA;
+        this.valueB=valueB;
+        return this.valueA + this.valueB;
+    }
 }
 
-let listOfnames2= names.map(item=> console.log(item.name))
+const calc = new calculator();
+console.log(calc.sum(2,2));
 
-const listoOfnames3=(name,age,country)=>{
-    ...
-}
+import {hello} from './module';
+hello();
 
-consta listOfnames4=> name {
-    ...
-}
+function* helloWorld(){
+    if (true){
+        yield 'hello, ';
+    }
+    if (true) {
+        yield 'World';
+    }
+};
+const generatorHello= helloWorld();
 
-const square = num=> num*num;
-
-//promesas
-
-const helloPromise=() =>{
-    return new Promise((resolve,reject)=>{
-        if (true){
-            resolve('hey');
-        }else{
-            reject('ups');
-        }
-    });
-}
-
-helloPromise()
-.then(response=>console.log(response))
-.then(()=> console.log('hola'))
-.catch(error => console.log(error));
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
